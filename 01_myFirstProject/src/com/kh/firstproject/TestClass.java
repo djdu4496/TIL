84da89189a1a544fe760a230d285e74ec266dc62
djdu4496 <55142939+djdu4496@users.noreply.github.com>
2020-01-06 02:50:26 +0900

    initial commit

---
+++ 01_myFirstProject/src/com/kh/firstproject/TestClass.java
@@ -0,0 +1,16 @@
+package com.kh.firstproject;
+
+public class TestClass {
+	
+	public static void main(String []orgs) {
+		
+			System.out.println("Hello World");	// (주석사용법)출력하는 문장
+			// 한줄 주석-------------------------
+			/*
+			 * 모두
+			 * 안녕하세요
+			 * 주석입니다
+			 * 오늘은 본격적으로 코딩을 배우는 날입니다. */
+	}
+}
+
