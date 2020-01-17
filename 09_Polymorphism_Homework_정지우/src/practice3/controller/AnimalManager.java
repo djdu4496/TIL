2e26422b03dc0116f1514509fd8bbab90b5b44a7
djdu4496 <55142939+djdu4496@users.noreply.github.com>
2020-01-17 14:14:21 +0900

    commit 20-17-01 Polymorphism(abstract class) practice

---
+++ 09_Polymorphism_Homework_정지우/src/practice3/controller/AnimalManager.java
@@ -0,0 +1,29 @@
+package practice3.controller;
+
+import practice3.model.vo.Animal;
+import practice3.model.vo.Cat;
+import practice3.model.vo.Dog;
+
+public class AnimalManager {
+	public static void main(String[] args) {
+		
+		// 사이즈 5짜리 Animal 타입의 객체 배열 생성
+		Animal[] aArr = new Animal[5];
+		
+		// 각 인덱스별로 무작위로 Dog, Cat 클래스를 이용하여 객체 생성
+		// 이때,매개변수 생성자를 이용하여 생성
+		
+		aArr[0] = new Dog("바둑이","말티즈",8);
+		aArr[1] = new Dog("지니","푸들",2);
+		aArr[2] = new Cat("애용이","누왁","좀비딸 세계관","황토색");
+		aArr[3] = new Cat("떼껄룩","흑고양이","좀비딸 세계관","검회색");
+		aArr[4] = new Dog("베일","요크셔",3);				
+				
+		// 반복문으로 각 인덱스별 객체의 speak() 메소드 실행
+		for(int i=0;i<aArr.length;i++) {
+				System.out.print(aArr[i].toString());
+				aArr[i].speak();
+		}
+		
+	}
+}
---
+++ 09_Polymorphism_Homework_정지우/src/practice3/model/vo/Dog.java
@@ -0,0 +1,26 @@
+package practice3.model.vo;
+
+public class Dog extends Animal {
+	public String PLACE = "애견카페";
+	private int weight;
+	
+	public Dog() {}
+	
+	public Dog(String name, String kinds, int weight) {
+		super(name,kinds);
+		this.weight=weight;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	
+	public void setWeight(int weight) {
+		this.weight=weight;
+	}
+	
+	public void speak() {
+		// 부모클래스 오버라이딩 해서 구현. 부모의 toString() 메소드 호출하여 리턴 받은 결과값 뒤에 “ 몸무게는 ” +weight + “kg 입니다.” 합쳐서 출력
+		System.out.println(super.toString() + " 몸무게는 " + weight + "kg 입니다.");
+	}
+}
